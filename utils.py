f3f8d11ade66d49e232ab72eb38684fb00dd9285
Gabriel Nunes <94002497+Gabriel7763@users.noreply.github.com>
2024-01-04 15:22:54 -0300

    funções concluidas

--- utils.py
+++ utils.py
@@ -1,39 +1,110 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+from firebase_admin import exceptions
+
+
 def conectar():
     """
     Função para conectar ao servidor
     """
-    print('Conectando ao servidor...')
+    cred_obj = credentials.Certificate("path/to/serviceAccountKey.json")
+    firebase_admin.initialize_app(cred_obj)
 
-def desconectar():
-    """ 
-    Função para desconectar do servidor.
-    """
-    print('Desconectando do servidor...')
+    db = firestore.client()
+    return db
 
 
-def listar():
+def listar_usuarios():
     """
     Função para listar os produtos
     """
-    print('Listando produtos...')
+    db = conectar()
 
-def inserir():
+    if db:
+        usuarios = db.collection("usuarios").get()
+        # se existir usuário
+        if usuarios:
+            print('Listando usuários...')
+            print('--------------------')
+            for usuario in usuarios:
+                print(f'ID: {usuario.id}')
+                print(f"Nome: {usuario.get('nome')}")
+                print(f"Email: {usuario.get('email')}")
+                print('---------------------')
+        else:
+            print('Não existem usuários cadastrados')
+    else:
+        print('Não foi possível conectar com o sevidor')
+
+
+def criar_usuario():
     """
     Função para inserir um produto
-    """  
-    print('Inserindo produto...')
+    """
+    db = conectar()
 
-def atualizar():
+    if db:
+        nome = input('Informe o nome do usuário: ')
+        email = input('Informe o email do usuário: ')
+
+        try:
+            usuario = db.collection('usuarios').add(
+                {"nome": nome, "email": email})
+
+            if usuario:
+                print(f'O usuário {nome} foi inserido com sucesso!')
+            else:
+                print('Não foi possível criar o usuário')
+        except exceptions.FirebaseError as e:
+            print(f'Não foi possível criar o usuário: {e}')
+    else:
+        print('Não foi possível conectar com o sevidor')
+
+
+def atualizar_usuario():
     """
     Função para atualizar um produto
     """
-    print('Atualizando produto...')
+    db = conectar()
 
-def deletar():
+    if db:
+        id_usuario = input('Informe o id do usuário: ')
+        # validando existencia do usuário
+        documento = db.collection('usuarios').document(id_usuario).get()
+        if not documento.exists:
+            print('Usuário não existe')
+        else:
+            nome = input('Informe o nome do usuário: ')
+            email = input('Informe o email do usuário: ')
+
+            novos_dados = ({'nome': nome, 'email': email})
+            try:
+                usuario = db.collection('usuarios').document(id_usuario)
+                usuario.update(novos_dados)
+                print('Dados do usuário foram atualizados')
+            except exceptions.FirebaseError as e:
+                print(f'Não foi possível atualizar o usuário: {e}')
+    else:
+        print('Não foi possível conectar com o sevidor')
+
+
+def deletar_usuario():
     """
     Função para deletar um produto
-    """  
-    print('Deletando produto...')
+    """
+    db = conectar()
+    id_usuario = input('Informe o ID do usuário: ')
+
+    if db:
+        try:
+            usuario = db.collection('usuarios').document(id_usuario)
+            usuario.delete()
+            print('Usuário deletado com sucesso!')
+        except exceptions.FirebaseError as e:
+            print(f'Não foi possível deletar o usuário: {e}')
+    else:
+        print('Erro ao conectar ao servidor')
+
 
 def menu():
     """
@@ -41,20 +112,20 @@ def menu():
     """
     print('=========Gerenciamento de Produtos==============')
     print('Selecione uma opção: ')
-    print('1 - Listar produtos.')
-    print('2 - Inserir produtos.')
-    print('3 - Atualizar produto.')
-    print('4 - Deletar produto.')
+    print('1 - Listar usuários.')
+    print('2 - Criar usuário.')
+    print('3 - Atualizar usuário.')
+    print('4 - Deletar usuário.')
     opcao = int(input())
     if opcao in [1, 2, 3, 4]:
         if opcao == 1:
-            listar()
+            listar_usuarios()
         elif opcao == 2:
-            inserir()
+            criar_usuario()
         elif opcao == 3:
-            atualizar()
+            atualizar_usuario()
         elif opcao == 4:
-            deletar()
+            deletar_usuario()
         else:
             print('Opção inválida')
     else:
